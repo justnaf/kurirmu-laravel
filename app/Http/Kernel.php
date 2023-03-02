619748964f6b01e3ff74db5a770cd37e6654d1b3
Naufal Anis F <naufalaf86@gmail.com>
2023-03-02 21:08:22 +0700

    fix kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,4 +64,9 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+    
+    protected $routeMiddleware = [
+        'user' => \App\Http\Middleware\UserAuthenticated::class,
+        'admin' => \App\Http\Middleware\AdminAuthenticated::class,
+    ];
 }
