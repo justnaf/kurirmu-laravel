70bf0d883b2134e4a21ba7615307a84b6125fd03
Naufal Anis F <naufalaf86@gmail.com>
2023-03-05 16:35:19 +0700

    Add Migration entry

---
+++ database/migrations/2023_03_05_092316_create_entry_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entry', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('data_id');
+            $table->foreign('data_id')->references('id')->on('data')->onDelete('cascade');
+            $table->text('no_telp');
+            $table->enum('status',['Dimiliki','Dijual','Rusak','Hilang','Meninggal','Pailit','Tidak Diketahui']);
+            $table->text('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entry');
+    }
+};
